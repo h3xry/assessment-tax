63bc919ec734f560ee16b02ff16166f3ef8aee58
h3xry <h3xryx3+github@gmail.com>
2024-04-24 16:08:32 +0700

    get env with viper lib

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"fmt"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.AutomaticEnv()
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", viper.GetString("PORT"))))
 }
